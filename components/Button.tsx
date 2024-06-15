afcd18f5dc9e67b60c66985e870c6e6f3abc288c
Egemen <egemenc2101@gmail.com>
2024-06-15 13:40:56 +0300

    end of the feature section and the footer

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,17 +6,18 @@ interface ButtonProps {
   icon?: string;
   variant: string;
   onClick?: () => void;
+  full?: boolean;
 }
 
-function Button({type, title, icon, variant, onClick}: ButtonProps) {
+function Button({type, title, icon, variant, onClick, full}: ButtonProps) {
   return (
       <button
           type={type}
-          className={`flexCenter gap-3 rounded-full border ${variant}`}
+          className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}` }
           onClick={onClick}
       >
         {icon && <Image src={icon} alt="title" width={24} height={24}/>}
-        <label className="bold-16 whitespace-nowrap">{title}</label>
+        <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
       </button>
   );
 }
--- components/Camp.tsx
+++ components/Camp.tsx
@@ -72,7 +72,8 @@ function Camp() {
             That is why we are here for those of you who want to start an
             adventure
           </p>
-          <Image src={'/quote.svg'} alt={"camp-2"} width={186} height={219} className={"camp-quote"}/>
+          <Image src={'/quote.svg'} alt={"camp-2"} width={186} height={219}
+                 className={"camp-quote"}/>
         </div>
       </div>
     </section>
